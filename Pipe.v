b5cca4392316ac28575812763f9070082bc0deb3
Antônio Almeida <toninetocontato@gmail.com>
2021-03-21 20:26:16 -0300

    edição5

--- Pipe.v
+++ Pipe.v
@@ -17,15 +17,15 @@ module Pipe (clk , reset, resultExecULA);
     wire [8:0] controlBits;
     wire hazard;
 
-    IF i_f (clk, reset, hazard, resultAddExec, EX_MEM_PCSrc, IF_ID_IR, IF_ID_NPC);  // clock, EX_MEM_NPC, IF_ID_IR, IF_ID_NPC
-    ID i_d (clk, reset, hazard, IF_ID_IR, IF_ID_NPC, writeReg, MEM_WB_Writereg, MEM_WB_Writedata, // Entradas
-                            controlBitsDec, numPcDec, rdataOut1Dec, rdataOut2Dec, extendOutDec, instrOut2016Dec, instrOut1511Dec, opCode, rs, rt, controlBits);  // Saídas
-    EX execute (clk, reset, controlBitsDec, numPcDec, rdataOut1Dec, rdataOut2Dec, extendOutDec, instrOut2016Dec, instrOut1511Dec, opCode,  fowardEA, fowardEB, MEM_WB_Writedata, // Entradas
+    IF i_f (clk, reset, hazard, resultAddExec, EX_MEM_PCSrc, IF_ID_IR, IF_ID_NPC); 
+    ID i_d (clk, reset, hazard, IF_ID_IR, IF_ID_NPC, writeReg, MEM_WB_Writereg, MEM_WB_Writedata,
+                            controlBitsDec, numPcDec, rdataOut1Dec, rdataOut2Dec, extendOutDec, instrOut2016Dec, instrOut1511Dec, opCode, rs, rt, controlBits);
+    EX execute (clk, reset, controlBitsDec, numPcDec, rdataOut1Dec, rdataOut2Dec, extendOutDec, instrOut2016Dec, instrOut1511Dec, opCode,  fowardEA, fowardEB, MEM_WB_Writedata,
                             controlBitsExec, resultAddExec, zeroExec, resultExecULA, rdataOut2Exec, muxOut5bExec);
-    MEM memory (clk, reset, controlBitsExec, resultExecULA, rdataOut2Exec, muxOut5bExec, // Entradas
-                            controlBitsMem, rdataOutMem, resultMemULA, MEM_WB_Writereg); // Saídas
+    MEM memory (clk, reset, controlBitsExec, resultExecULA, rdataOut2Exec, muxOut5bExec,
+                            controlBitsMem, rdataOutMem, resultMemULA, MEM_WB_Writereg);
     WB writeback (controlBitsMem, rdataOutMem, resultMemULA, MEM_WB_Writedata);
-    hazard1 hzrd(IF_ID_IR[25:21], IF_ID_IR[20:16], rt, controlBitsDec[3], hazard); // (RsD, RtD, RtE, Memread, hazard);
+    hazard1 hzrd(IF_ID_IR[25:21], IF_ID_IR[20:16], rt, controlBitsDec[3], hazard);
     
     fowarding fwrd(rs, rt, muxOut5bExec, controlBitsExec[1], MEM_WB_Writereg, controlBitsMem[1], fowardEA, fowardEB);
 
--- WB.v
+++ WB.v
@@ -1,9 +1,7 @@
-
-
-module WB(bitsCtr, read_data, ALUmem, wb_data);
-    input [1:0] bitsCtr;
-    input [31:0] read_data, ALUmem;
-    output wire [31:0] wb_data;
+module WB(controlBits, dataRead, memULA, dataWB);
+    input [1:0] controlBits;
+    input [31:0] dataRead, memULA;
+    output wire [31:0] dataWB;
     
-    MUX muxwb (read_data, ALUmem, bitsCtr[0] /*MemToReg*/, wb_data);
+    MUX wbmux (dataRead, memULA, controlBits[0], dataWB);
 endmodule
